b80a312b33f9079286e221ad823a0415afe9a288
UrsusFeline . <ursusfeline07@gmail.com>
2024-10-19 23:50:00 +0800

    fix

--- render.sh
+++ render.sh
@@ -9,26 +9,24 @@ FILE_LIST=$(ls -1 $SOURCE)
 for FILE in $FILE_LIST;
     do
     echo "## $FILE" > $DESTINATION/$FILE.md
-    echo "|EN_US|ZH_CN|" >> $DESTINATION/$FILE.md
-    echo "|:----|:----|" >> $DESTINATION/$FILE.md
+    echo "|EN_US|ZH_CN|  " >> $DESTINATION/$FILE.md
+    echo "|:----|:----|  " >> $DESTINATION/$FILE.md
     for LINE in $(cat $SOURCE/$FILE);
         do
-            echo "|$LINE|" >> $DESTINATION/$FILE.md
+            echo "|$LINE|  " >> $DESTINATION/$FILE.md
         done
-    cat $DESTINATION/$FILE.md | sed 's/$/\r/' > $DESTINATION/$FILE.md
     done
 
 ## Generate index page.
 cat README.md > $DESTINATION/index.md
 echo "## 期刊列表" >> $DESTINATION/index.md
-echo "|期数|日期|" >> $DESTINATION/index.md
-echo "|:--|:--|" >> $DESTINATION/index.md
+echo "|期数|日期|  " >> $DESTINATION/index.md
+echo "|:--|:--|  " >> $DESTINATION/index.md
 for FILE in $FILE_LIST;
     do
-        echo "|[${FILE%.*}](./$FILE)|" >> $DESTINATION/index.md
+        echo "|[${FILE%.*}](./$FILE)|  " >> $DESTINATION/index.md
     done
 echo "## 使用协议" >> $DESTINATION/index.md
 echo '```' >> $DESTINATION/index.md
 cat LICENSE >> $DESTINATION/index.md
 echo '```' >> $DESTINATION/index.md
-cat $DESTINATION/index.md | sed 's/$/\r/' > $DESTINATION/index.md
