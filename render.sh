409472c4dc1d0c69d4333eb4d07fd1f4f61e785e
UrsusFeline . <ursusfeline07@gmail.com>
2024-10-19 23:40:29 +0800

    fix

--- render.sh
+++ render.sh
@@ -13,13 +13,15 @@ for FILE in $FILE_LIST;
     echo "|:----|:----|" >> $DESTINATION/$FILE'.md'
     for LINE in $(cat $SOURCE/$FILE);
         do
-            echo "'|'$LINE'|'" >> $DESTINATION/$FILE'.md'
+            echo "|$LINE|" >> $DESTINATION/$FILE'.md'
         done
     done
 
 ## Generate index page.
 cat README.md > $DESTINATION/index.md
 echo "## 期刊列表" >> $DESTINATION/index.md
+echo "|期数|日期|"
+echo "|:--|:--|"
 for FILE in $FILE_LIST;
     do
         echo "|[${FILE%.*}](./$FILE)|" >> $DESTINATION/index.md
